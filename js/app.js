db7216282f5122a48727c978e537c65c31920193
Riley Adair <rileydadair@gmail.com>
2017-11-17 15:07:12 -0600

    updated links

--- js/app.js
+++ js/app.js
@@ -39,12 +39,10 @@ $(document).ready(function(){
       var element_top_position = $element.offset().top;
       var element_bottom_position = (element_top_position + element_height);
 
-      //check to see if this current container is within viewport
       if ((element_bottom_position >= window_top_position) &&
           (element_top_position <= window_bottom_position)) {
         $element.addClass('in-view-two');
       }
-      // else
     });
   }
 
