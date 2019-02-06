58f76ac6d7c83e24de96f115ded17244d5ccd469
Riley Adair <hello@rileyadair.com>
2019-02-06 12:59:58 -0600

    Decrease vs mouseMultiplier

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -17,7 +17,7 @@ export default {
         preload: false,
         native: false,
         section: document.querySelector('.main'),
-        vs : { mouseMultiplier: 0.8 },
+        vs : { mouseMultiplier: 0.6 },
       });
     } else {
       this.initMobileEvents();
